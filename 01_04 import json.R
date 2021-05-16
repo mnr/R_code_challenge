1414be8f8a409e3f45278eae1725d855ea53ea6f
Mark Niemann-Ross <mark.niemannross@gmail.com>
2021-05-16 11:06:59 -0700

    import JSON

---
+++ 01_04 import json.R
@@ -0,0 +1,19 @@
+# challenge: retrieve the most current water level
+# error trap the retrieval
+# extract both the level and date from results
+
+#install.packages("jsonlite")
+library(jsonlite)
+
+bullrunURL <- "https://waterservices.usgs.gov/nwis/iv/?format=json&sites=14138850&parameterCd=00060,00065&siteStatus=all"
+
+bullrunWater <- tryCatch(fromJSON(bullrunURL),
+                         error = function(e) {message(paste("Error:", e))},
+                         warning = function(w) {message(paste("Warning:", w))},
+                         message = function(m) {message(paste("Message:", m))}, 
+                         interrupt = function(i) {message(paste("Interrupted:",i))}, 
+                         condition = function(c) {message(paste("Condition:",c))}
+)
+
+brw_data <- as.numeric(bullrunWater$value$timeSeries$values[[1]]$value[[1]]$value)
+brw_time <- as.Date(bullrunWater$value$timeSeries$values[[1]]$value[[1]]$dateTime )
