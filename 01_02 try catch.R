7388a36f4df8a8d592c3f8d719ae19c3133a5afc
Mark Niemann-Ross <mark.niemannross@gmail.com>
2021-05-15 23:40:32 -0700

    import csv and try catch

---
+++ 01_02 try catch.R
@@ -0,0 +1,22 @@
+# Use try catch to error trap a URL
+
+# Challenge: import the United Nations world population database
+# all fields should be integer or numeric
+# except variant = factor, location = character
+# If the URL doesn't work, import a local copy of the file
+
+# the URL to use is
+# "https://population.un.org/wpp/Download/Files/1_Indicators%20(Standard)/CSV_FILES/WPP2019_TotalPopulationBySex.csv"
+
+worldPopURLBase <- "https://population.un.org/wpp/Download/Files/1_Indicators%20(Standard)/CSV_FILES/"
+worldPopFile <- "WPP2019_TotalPopulationBySex.csv"
+worldPopURL <- paste0(worldPopURLBase, worldPopFile)
+worldPopColClasses <- c("integer","character","integer", "factor", "integer", "numeric","numeric","numeric","numeric","numeric") 
+
+worldPop <- tryCatch(read.csv(worldPopURL,colClasses = worldPopColClasses ),
+                     error = function(e) {
+                       message(paste("The error is:", e))
+                       read.csv(worldPopFile,colClasses = worldPopColClasses )
+                     }
+                     )
+
