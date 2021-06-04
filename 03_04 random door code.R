7f17019ae7861c5f9c4beaa4b1d0fbad2fddf931
Mark Niemann-Ross <mark.niemannross@gmail.com>
2021-06-04 14:56:07 -0700

    renumber and add progress statements

--- 03_04 random door code.R
+++ 03_04 random door code.R
@@ -5,7 +5,7 @@
 # ...hard to guess
 # ...looks like a date - YYMMDD
 # ...always six digits
-# ...only uses each digit (0-9) once in each code
+# ...all digits in code are unique
 # ...different than all preceding door codes
 
 tryCatch(load(file="previousDoorCodes.rds"),
@@ -13,15 +13,19 @@ tryCatch(load(file="previousDoorCodes.rds"),
 )
 
 candidate_split <- c(1,1)
+candidate_sixdigits <- candidate_split
 
-while (length(unique(candidate_split)) != length(candidate_split)) {
+while (length(unique(candidate_split)) != 6) {
+  print(paste("failed:", candidate_sixdigits))
   candidate_datetime <- sample(seq(as.Date('1900/01/01'), Sys.Date(), by="day"), 1)
   candidate_sixdigits <- format(candidate_datetime, format = "%y%m%d")
   if (candidate_sixdigits %in% previousDoorCodes) {
-    candidate_sixdigits <- c(1,1)
+    candidate_split <- c(1,1)
   } else 
     { candidate_split <- unlist(strsplit(candidate_sixdigits, split = ""))}
 }
+
+print(paste("Succeed:", candidate_sixdigits))
 
 previousDoorCodes <- c(previousDoorCodes,candidate_sixdigits)
 save(previousDoorCodes, file = "previousDoorCodes.rds")
