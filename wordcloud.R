c0c7591fd5846f64a75dce2f641df43a6c52e059
Mark Niemann-Ross <mark.niemannross@gmail.com>
2021-06-13 21:07:01 -0700

    clean up plot

--- wordcloud.R
+++ wordcloud.R
@@ -2,21 +2,31 @@
 
 # install.packages("wordcloud")
 library(wordcloud)
+library(readxl)
 
-# get data
+
+# get world population data and list of countries ----------------
 source("01_02 try catch.R")
-# metadata - https://population.un.org/wpp/Download/Files/4_Metadata/WPP2019_F01_LOCATIONS.XLSX
+# get metada about the worldpop database
+worldPopMeta <- as.data.frame(read_excel("WPP2019_F01_LOCATIONS.XLSX",
+                                         sheet = "Location"))
+worldPopMeta <- worldPopMeta[ worldPopMeta$...7 %in% "Country/Area", "...2"]
 
-
-# select 
+# subset the data to Location, PopTotal, and PopDensity --------------------
 finalData <- worldPop[ worldPop$Time == 2021 & 
-                         worldPop$Variant == "Medium",
+                         worldPop$Variant == "Medium" &
+                         worldPop$Location %in% worldPopMeta,
                        c("Location", "PopTotal","PopDensity") ]
 
+# assign colors (red to blue) Red = large pop density
 myColors <- colorRampPalette(c("Red","Blue"))
 
-finalData <- finalData[order(finalData$PopDensity, decreasing = TRUE),]
+finalData <- finalData[order(finalData$PopDensity, decreasing = TRUE), ]
+
 finalData$thisRowColor <- myColors(nrow(finalData))
 
-wordcloud(words=finalData$Location, finalData$PopTotal, 
+# create the word cloud -----------------
+par(mar = c(1,1,1,1), pty="m")
+wordcloud(words=finalData$Location, 
+          freq=finalData$PopTotal, 
           colors = finalData$thisRowColor)
